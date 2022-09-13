45e1c1be41b5eceb06d967df931f0cbc666d9362
NhuNgoc1101 <113379960+NhuNgoc1101@users.noreply.github.com>
2022-09-13 13:40:12 +0700

    Add files via upload

---
+++ Tuan 3/Tuan3-code.vb
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>BT tuan 3</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+      </head>
+   <style>
+    .container{
+            text-align: center;
+            line-height: 25px;
+            font-family: Arial, sans-serif;
+            border-radius: 5px;
+            margin: 25px;
+            border: 1px solid black;
+           
+           
+            }
+   </style>
+    <body>
+        <h2><center>Kiều Như Ngọc-B19DCDT160</h2>
+        <div class="container ">
+            <div class="row">
+                <div class="col card bg-dark text-white p-3 m-2" >
+                    <h2>NHIỆT ĐỘ</h2>
+                    <p>Dữ liệu nhiệt độ...</p>
+                    
+                </div>
+                <div class="col card bg-dark text-white p-3 m-2 " >
+                    <h2>ĐỘ ẨM</h2>
+                    <p>Dữ liệu độ ẩm...</p>
+                </div>
+                <div class="col card bg-dark text-white p-3 m-2 ">
+                    <h2>ÁNH SÁNG</h2>
+                    <p>Dữ liệu ánh sáng...</p>
+                    
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-8">
+                    <img src="https://www.luyenthithukhoa.vn/attachments/01-png.41510/">
+                </div>
+                <div class="col-4" >
+                    <button type="button" class="btn btn-success"> ON</button>
+                    <button type="button" class="btn btn-danger">OFF</button>
+               </div>
+            </div>
+            
+        </div>
+    </body>
+</html>
